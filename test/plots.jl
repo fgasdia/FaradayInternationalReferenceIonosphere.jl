5388a028f77990dc42f601c54df231c96a723082
fgasdia <17058025+fgasdia@users.noreply.github.com>
2021-02-26 00:30:42 -0700

    Save plot figures (but results aren't tracked)

--- test/plots.jl
+++ test/plots.jl
@@ -14,6 +14,7 @@ plot(day_mean, alt/1000, label="mean", xscale=:log10, linewidth=2, ylims=(55, 11
 plot!(day_med, alt/1000, label="median", xscale=:log10, linewidth=2)
 plot!(day_30, alt/1000, label="30%", xscale=:log10)
 plot!(day_70, alt/1000, label="70%", xscale=:log10)
+savefig("day_mean.png")
 
 night_mean = firi(chi=(100, 180))
 night_med = FIRITools.quantile(0.5, chi=(100, 180))
@@ -25,6 +26,7 @@ plot(night_mean, alt/1000, label="mean", xscale=:log10, linewidth=2, ylims=(55, 
 plot!(night_med, alt/1000, label="median", xscale=:log10, linewidth=2)
 plot!(night_30, alt/1000, label="30%", xscale=:log10)
 plot!(night_70, alt/1000, label="70%", xscale=:log10)
+savefig("night_mean.png")
 
 # Compare mean and median around interpolation points
 day_mean = firi(chi=(0, 95), lat=(30, 45))
@@ -37,6 +39,7 @@ plot(day_mean, alt/1000, label="mean", xscale=:log10, linewidth=2, ylims=(55, 11
 plot!(day_med, alt/1000, label="median", xscale=:log10, linewidth=2)
 plot!(day_30, alt/1000, label="30%", xscale=:log10)
 plot!(day_70, alt/1000, label="70%", xscale=:log10)
+savefig("day_mean_latzoom.png")
 
 day_mean = firi(chi=(30, 45))
 day_med = FIRITools.quantile(0.5, chi=(30, 45))
@@ -48,36 +51,71 @@ plot(day_mean, alt/1000, label="mean", xscale=:log10, linewidth=2, ylims=(55, 11
 plot!(day_med, alt/1000, label="median", xscale=:log10, linewidth=2)
 plot!(day_30, alt/1000, label="30%", xscale=:log10)
 plot!(day_70, alt/1000, label="70%", xscale=:log10)
+savefig("day_mean_chizoom.png")
 
 # Check to see if interpolation is acting as expected
 
 real_lats = unique(FIRITools.HEADER[!,"Lat, deg"])
+lats = 0:5:90
+interp_lat = setdiff(lats, real_lats)
+N = length(lats)
+cmap = palette(:rainbow, N, rev=true)
 
 p = plot(ylabel="altitude (km)", xlabel="Ne (m⁻³)", xscale=:log10, legend=:topleft,
-         title="Latitude", ylims=(55, 110))
+         ylims=(55, 110), legendtitle="Latitude")
 for la in real_lats
     prof = firi(45, la)
-    plot!(p, prof, alt/1000, label=la)
+    plot!(p, prof, alt/1000, label=la, color=cmap[findfirst(isequal(la), lats)])
+end
+for la in interp_lat
+    prof = firi(45, la)
+    plot!(p, prof, alt/1000, label=nothing, color=cmap[findfirst(isequal(la), lats)],
+          linestyle=:dash)
 end
 display(p)
-
+savefig("alt.png")
 
 real_sza = unique(FIRITools.HEADER[!,"Chi, deg"])
-interp_sza = setdiff(0:5:180, real_sza)
-N = length(0:5:180)
-cmap = palette(:rainbow, N)
+chis = 0:5:130
+interp_sza = setdiff(chis, real_sza)
+# interp_sza = [105, 110, 115, 120]
+N = length(chis)
+cmap = palette(:rainbow, N, rev=true)
 
-p = plot(ylabel="altitude (km)", xlabel="Ne (m⁻³)", xscale=:log10, legend=false,
-         title="SZA", ylims=(55, 110))
+p = plot(ylabel="altitude (km)", xlabel="Ne (m⁻³)", xscale=:log10,
+         ylims=(55, 110), legend=:topleft, legendtitle="SZA")
 for sza in real_sza
     prof = firi(sza, 45)
-    plot!(p, prof, alt/1000, label=sza, color=cmap[findfirst(isequal(sza), 0:5:180)])
+    plot!(p, prof, alt/1000, label=sza, color=cmap[findfirst(isequal(sza), chis)])
 end
 
 for sza in interp_sza
     prof = firi(sza, 45)
-    plot!(p, prof, alt/1000, label=sza, color=cmap[findfirst(isequal(sza), 0:5:180)],
+    plot!(p, prof, alt/1000, label=nothing, color=cmap[findfirst(isequal(sza), chis)],
           linestyle=:dash)
 end
-
 display(p)
+savefig(p, "sza.png")
+
+
+real_sza = [75, 80, 85, 90, 95, 100]
+chis = 75:100
+interp_sza = setdiff(chis, real_sza)
+# interp_sza = 89
+N = length(chis)
+cmap = palette(:rainbow, N, rev=true)
+
+p = plot(ylabel="altitude (km)", xlabel="Ne (m⁻³)", xscale=:log10,
+         ylims=(55, 110), legend=:topleft, legendtitle="SZA")
+for sza in real_sza
+    prof = firi(sza, 45)
+    plot!(p, prof, alt/1000, label=sza, color=cmap[findfirst(isequal(sza), chis)])
+end
+
+for sza in interp_sza
+    prof = firi(sza, 45)
+    plot!(p, prof, alt/1000, label=nothing, color=cmap[findfirst(isequal(sza), chis)],
+          linestyle=:dash)
+end
+display(p)
+savefig("sza_zoom.png")
